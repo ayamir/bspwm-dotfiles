700ff9a674451677f4de50080f2fca3b56f94bd3
MiraculousMoon <lgt986452565@gmail.com>
2020-06-16 23:36:43 +0800

    add just two wallpapers

--- Background/auto-change.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh  
-while true; do  
-  arr=(`find /home/mir/Pictures/Background -type f \( -name '*.jpg' -o -name '*.png' \)|shuf -n2`)  
-  bg0=${arr[0]}  
-  #feh --bg-fill $bg0 --bg-fill $bg1  
-  feh --bg-scale $bg0 --bg-scale $bg1  
-  sleep 30m  
-done
