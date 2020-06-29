1271b446da7cb2824b8e26d0e0447cd068af7e31
MiraculousMoon <lgt986452565@gmail.com>
2020-06-29 22:51:55 +0800

    modify top button color to adopt nord theme

---
+++ macBigSur/polybar/launch.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+if [ "$1" == "light" ]
+then
+	polybar -c $HOME/.config/polybar/light-config nord-top &
+	polybar -c $HOME/.config/polybar/light-config nord-down &
+else
+	polybar -c $HOME/.config/polybar/dark-config nord-top &
+	polybar -c $HOME/.config/polybar/dark-config nord-down &
+fi
+
+echo "Bars launched..."
