60bbb270345d60b72790813c56621da5687832ed
MiraculousMoon <lgt986452565@gmail.com>
2020-11-29 15:59:04 +0800

    daliy update

--- nord/bspwm/scripts/start_ides.sh
+++ nord/bspwm/scripts/start_ides.sh
@@ -6,34 +6,39 @@ then
     exit 0
 elif [ "$1" == "idea" ]
 then
-    sh ~/jetbrains/idea-IU-201.7846.76/bin/idea.sh
+    sh ~/.local/bin/idea
     exit 0
 elif [ "$1" == "clion" ]
 then
-    sh ~/jetbrains/clion-2020.1.2/bin/clion.sh
+    sh ~/.local/bin/clion
     exit 0
 elif [ "$1" == "pycharm" ]
 then
-    sh ~/jetbrains/pycharm-2020.1.2/bin/pycharm.sh
+    
     exit 0
 elif [ "$1" == "webstorm" ]
 then
-    sh ~/jetbrains/WebStorm-202.6948.73/bin/webstorm.sh
+    
     exit 0
 elif [ "$1" == "datagrip" ]
 then
-    sh ~/jetbrains/DataGrip-2020.1.4/bin/datagrip.sh
+    
     exit 0
 elif [ "$1" == "phpstorm" ]
 then
-    sh ~/jetbrains/PhpStorm-201.8538.41/bin/phpstorm.sh
+    
     exit 0
 elif [ "$1" == "android" ]
 then
-    flatpak run android-studio
+    sh ~/.local/bin/android-studio
     exit 0
 elif [ "$1" == "rider" ]
 then
     sh ~/jetbrains/Rider-2020.2.2/bin/rider.sh
     exit 0
+elif [ "$1" == "goland" ]
+then
+    sh ~/.local/bin/goland
+    exit 0
+fi
 fi
